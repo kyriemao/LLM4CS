87dbce864e22479a9a41b8732167ed96ae365754
kyriemao <kyriemao@foxmail.com>
2023-06-30 15:40:09 +0800

    add

--- scripts/run_prompt_rewrite_and_response.sh
+++
@@ -1,7 +0,0 @@
-python prompt_rewrite_and_response.py \
---open_ai_key_id=0 \
---demonstration_file_path="demonstrations.json" \
---test_file_path="./datasets/cast20_test.json" \
---work_dir="./results/cast20/RAR" \
---model_name="code-davinci-002" \
---n_completion=5 \
